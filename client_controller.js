8926a12ad16b3b8e3ccbb30bfb0c7b70d7976c60
Pascal Lefebvre <skrova2017@gmail.com>
2022-09-15 14:40:46 +0200

    Chargement de toutes les données dans la page d'accueil (reste à faire la mise en page et le carroussel).

--- client_controller.js
+++ client_controller.js
@@ -24,17 +24,57 @@ class Movie {
 class Category {
     constructor(genre) {
         this.genre = genre;
+        this.movies = [];
         this.imdbScoreMovies = [];
         this._intializeImdbScoreMovies();
     }
 
     _intializeImdbScoreMovies() {
         const emptyElement = {id: 0, imdbScore: 0, imageUrl: ''}
-        if (this.genre == 'All') { var elementsNumber = 8; } else { var elementsNumber = 7; }
+        if (this.genre == 'all') { var elementsNumber = numberOfMoviesByCategory + 1 } else { var elementsNumber = numberOfMoviesByCategory }
         for (let i = 0; i < elementsNumber; i++) {
             this.imdbScoreMovies.push(emptyElement);
         }
     }
+
+    addMovie(movie) {
+        this.movies.push(movie);
+    }
+}
+
+
+/* View */
+
+
+function updateHomePageData() {
+    // Load top movie data all categories
+    document.querySelector("#banniere_film img").src = categories.all.movies[0].image;
+    document.querySelector("#banniere_film h3").textContent = categories.all.movies[0].title;
+    document.querySelector("#banniere_film p").textContent = categories.all.movies[0].abstract;
+    //
+    const categoryImages = document.querySelectorAll(".category img");
+    let i = 0;
+    for (const category in categories) {
+        if (category == 'all') {
+            for (let j = 1; j < numberOfMoviesByCategory+1; j++) {
+                categoryImages[i].src = categories[category].imdbScoreMovies[j].imageUrl;
+                i++;
+            }
+        } else {
+            for (let j = 0; j < numberOfMoviesByCategory; j++) {
+                categoryImages[i].src = categories[category].imdbScoreMovies[j].imageUrl;
+                i++;
+            }
+        }
+    }
+    /*for (let catIndex in genres) {
+        if (catIndex == 0) { var i = 1 } else { var i = 0 };
+        var categoryImages = document.querySelectorAll(categoryHtmlId[catIndex]);
+        while (i < categories.genres[catIndex].imdbScoreMovies.length) {
+            categoryImages[i].src = categories.all.imdbScoreMovies[i].imageUrl;
+            i++;
+        }
+    }*/
 }
 
 
@@ -43,10 +83,21 @@ class Category {
 
 /* import { Category } from "./client_model"; */
 
-const categories = [ new Category('All'), new Category('Action'), new Category('Family'), new Category('Comedy') ];
+//const categoryHtmlId = [ "#top_rated img", "#action_top_rated img", "#family_top_rated img", "#comedy_top_rated img"];
+const numberOfMoviesByCategory = 7;
 const titlesUrl = "http://localhost:8000/api/v1/titles/";
-const firstUrls = [ titlesUrl + "?page=17150", titlesUrl + "?genre=Action&page=2570",
-                    titlesUrl + "?genre=Family&page=770", titlesUrl + "?genre=Comedy&page=5850" ];
+const firstUrls = {
+    all:    `${titlesUrl}?page=17160`,
+    action: `${titlesUrl}?genre=Action&page=2570`,
+    family: `${titlesUrl}?genre=Family&page=770`,
+    comedy: `${titlesUrl}?genre=Comedy&page=5850`
+};
+const categories = {
+    all: new Category('all'),
+    action: new Category('action'),
+    family: new Category('family'),
+    comedy: new Category('comedy')
+};
 
 async function getMoviesData(url) {
     try {
@@ -59,25 +110,25 @@ async function getMoviesData(url) {
     }
 }
 
-function rankBestMovies(index, movies) {
+function rankBestMovies(category, movies) {
     for (let movie of movies) {
-        for (let i in categories[index].imdbScoreMovies) {
-            if (movie.imdb_score > categories[index].imdbScoreMovies[i].imdbScore) {
+        for (let i in category.imdbScoreMovies) {
+            if (movie.imdb_score > category.imdbScoreMovies[i].imdbScore) {
                 let newElement = {id: movie.id, imdbScore: movie.imdb_score, imageUrl: movie.image_url};
-                categories[index].imdbScoreMovies.splice(i, 0, newElement);
-                categories[index].imdbScoreMovies.splice(categories[index].imdbScoreMovies.length-1);
+                category.imdbScoreMovies.splice(i, 0, newElement);
+                category.imdbScoreMovies.splice(category.imdbScoreMovies.length-1);
                 break;
             }
         }
     }
 }
 
-async function searchImdbScoresMax(genreIndex, firstUrl, lastUrl = null) {
+async function searchImdbScoresMax(category, firstUrl, lastUrl = null) {
     let nextUrl = firstUrl;
     while (nextUrl != lastUrl) {
         try {
             let value = await getMoviesData(nextUrl);
-            rankBestMovies(genreIndex, value.results);
+            rankBestMovies(category, value.results);
             nextUrl = value.next;
         } catch (error) {
             console.log(error);
@@ -86,34 +137,41 @@ async function searchImdbScoresMax(genreIndex, firstUrl, lastUrl = null) {
     }
 }
 
-async function storeMovieData(id) {
+async function storeMovieData(id, category) {
     let idUrl = titlesUrl + id;
     try {
         const value = await getMoviesData(idUrl);
-        return new Movie(value.id, value.image_url, value.title, value.genres, value.year, value.rated,
-                         value.imdbScore, value.directors, value.actors, value.duration, value.countries,
-                         value.worldwide_gross_income, value.long_description);
+        const movie = new Movie(value.id, value.image_url, value.title, value.genres, value.year,
+                                value.rated, value.imdbScore, value.directors, value.actors, value.duration,
+                                value.countries, value.worldwide_gross_income, value.long_description);
+        category.addMovie(movie);
     } catch (error) {
         console.log(error);
     }
 }
 
-function main() {
-    Promise.all([searchImdbScoresMax(0, firstUrls[0]), searchImdbScoresMax(1, firstUrls[1]),
-                searchImdbScoresMax(2, firstUrls[2]), searchImdbScoresMax(3, firstUrls[3])])
+function getAllDataForHomePage() {
+    Promise.all([searchImdbScoresMax(categories.all, firstUrls.all),
+                searchImdbScoresMax(categories.action, firstUrls.action),
+                searchImdbScoresMax(categories.family, firstUrls.family),
+                searchImdbScoresMax(categories.comedy, firstUrls.comedy)])
     .then(async () => {
-        const topMovie = await storeMovieData(categories[0].imdbScoreMovies[0].id);
-        console.log(topMovie.title);
-        for (let categorie of categories) {
-            console.log(categorie.genre);
-            for (let movie of categorie.imdbScoreMovies) {
+        /* for (let catKey in categories) {
+            console.log(categories[catKey].genre);
+            for (let movie of categories[catKey].imdbScoreMovies) {
                 console.log(`${movie.id} ${movie.imdbScore} ${movie.imageUrl}`);
             }
-        }
+        } */
+        await storeMovieData(categories.all.imdbScoreMovies[0].id, categories.all);
+        updateHomePageData();
     })
-    .catch(function(error) {
+    .catch((error) => {
         console.log(error);
     });
+}
+
+function main() {
+    getAllDataForHomePage();
 }
 
 main()
